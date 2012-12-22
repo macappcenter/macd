72444b9eac1f1033acc053f1c72f96d9c0219606
Alex Leigh <leigh@alexleigh.me>
2012-12-21 22:40:25 -0500

    Initial commit

---
+++ macd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  macd
+//
+//  Created by Alex Leigh on 12/21/12.
+//  Copyright (c) 2012 MAC App Center. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, launchd!\n");
+    return 0;
+}
+
